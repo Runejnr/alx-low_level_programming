26ec914f2fcb5ddd321358c9d489675c53fac632
Runejnr <61361407+Runejnr@users.noreply.github.com>
2022-09-14 07:51:53 +0800

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,17 +5,17 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void times_table(void);
 {
 
-n=int(input('Please enter a positive integer between 1 and 15: '))
+n=int(input('2: '))
 for row in range(1,n+1):
     for col in range(1,n+1):
         print(row*col, end="\t")
     print()
 
 
-n=int(input('Please enter a positive integer between 1 and 15: '))
+n=int(input('2: '))
 for row in range(1,n+1):
     print(*("{:3}".format(row*col) for col in range(1, n+1)))
 
