6a5800a366cc0d5381013471427d7fc479c8a056
Runejnr <61361407+Runejnr@users.noreply.github.com>
2022-09-14 07:52:41 +0800

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,14 @@
 void times_table(void);
 {
 
-n=int(input('2: '))
+n=int(input(1))
 for row in range(1,n+1):
     for col in range(1,n+1):
         print(row*col, end="\t")
     print()
 
 
-n=int(input('2: '))
+n=int(input(1))
 for row in range(1,n+1):
     print(*("{:3}".format(row*col) for col in range(1, n+1)))
 
