32661814b851a4e037525a7cc973172cce9beef8
Runejnr <61361407+Runejnr@users.noreply.github.com>
2022-09-14 08:23:24 +0800

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 98)
+	while (i < 87)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 90)
+		if (i < 87)
 			printf(", ");
 		i++;
 	}
